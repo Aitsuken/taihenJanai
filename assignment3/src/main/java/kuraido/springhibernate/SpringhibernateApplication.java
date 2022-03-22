66970a55aa0e35aad8aa1887c0841b579dc06f6c
Aitsuken <enderlordschannel@gmail.com>
2022-03-22 11:15:19 +0600

    addedAss3

---
+++ assignment3/src/main/java/kuraido/springhibernate/SpringhibernateApplication.java
@@ -0,0 +1,13 @@
+package kuraido.springhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringhibernateApplication.class, args);
+    }
+
+}
