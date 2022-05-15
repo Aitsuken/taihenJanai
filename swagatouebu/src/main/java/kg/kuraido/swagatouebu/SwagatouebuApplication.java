47248dda4819d03cc15c46c9ffa0945df74eae2a
Aitsuken <enderlordschannel@gmail.com>
2022-05-15 11:52:43 +0600

    added the project

---
+++ swagatouebu/src/main/java/kg/kuraido/swagatouebu/SwagatouebuApplication.java
@@ -0,0 +1,13 @@
+package kg.kuraido.swagatouebu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwagatouebuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwagatouebuApplication.class, args);
+    }
+
+}
---
+++ swagatouebu/src/test/java/kg/kuraido/swagatouebu/SwagatouebuApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kuraido.swagatouebu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwagatouebuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
